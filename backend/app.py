f72b2753bfafbce2a09a32f67bcb34691b16ac2d
Enock Walasi <91232899+waldis4u@users.noreply.github.com>
2025-06-08 18:39:59 +0000

    Fix deployment issues

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import os
+import uvicorn
 
 app = FastAPI()
 
@@ -30,7 +31,7 @@ async def read_root(request: Request):
 def start_server():
     port = int(os.environ.get("PORT", 8000))
     print(f"Starting server on port {port}")  # Important for debugging
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 if __name__ == "__main__":
     start_server()
